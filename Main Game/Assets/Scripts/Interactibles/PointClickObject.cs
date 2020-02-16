08dfe39a24ed6ea19498d73458dc799e52ceb5e0
devin-ohlson <46634476+devin-ohlson@users.noreply.github.com>
2020-02-16 15:12:32 -0500

    Dohlson/player controller (#16)
    * Set up a mouse and keyboard player controller prefab.
    Initialized input bindings for the Vive controllers.
    
    * Updated scene for clearer examples
    
    * Added clickable interactive objects (e.g. buttons)
    Set up a demo for that in the MouseKeyboardScene

---
+++ Main Game/Assets/Scripts/Interactibles/PointClickObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+//Used for any object that the user can point at and interact with
+[RequireComponent(typeof(Collider))]
+public abstract class PointClickObject : MonoBehaviour {
+	private Vector3 originalScale;
+	[SerializeField] private float scaleAmount;
+
+	protected virtual void Start() {
+		originalScale = transform.localScale;
+	}
+
+	public virtual void Highlight() {
+		StartCoroutine(HighlightCoroutine());
+	}
+
+	protected virtual IEnumerator HighlightCoroutine() {
+		transform.localScale *= scaleAmount;
+		yield return new WaitForEndOfFrame();
+		transform.localScale = originalScale;
+	}
+
+	public abstract void Interact(HandController hand);
+}
---
+++ Main Game/Assets/Scripts/VRInputManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class VRInputManager : MonoBehaviour {
+	public SteamVR_Action_Boolean triggerAction, gripAction, menuAction;
+	public SteamVR_Action_Boolean leftClickAction, rightClickAction, upClickAction, downClickAction;
+
+	public bool GetTrigger() {
+		return GetTrigger(SteamVR_Input_Sources.Any);
+	}
+	public bool GetTrigger(SteamVR_Input_Sources hand) {
+		bool mouseClick = false;
+		switch (hand) {
+			case SteamVR_Input_Sources.LeftHand:
+				mouseClick = Input.GetMouseButton(0);
+				break;
+			case SteamVR_Input_Sources.RightHand:
+				mouseClick = Input.GetMouseButton(1);
+				break;
+			case SteamVR_Input_Sources.Any:
+				mouseClick = Input.GetMouseButton(0) || Input.GetMouseButton(1);
+				break;
+		}
+
+		return triggerAction.GetStateDown(hand) || mouseClick;
+	}
+	public bool GetGrip() {
+		return gripAction.GetStateDown(SteamVR_Input_Sources.Any);
+	}
+	public bool GetGrip(SteamVR_Input_Sources hand) {
+		return gripAction.GetStateDown(hand);
+	}
+	public bool GetMenu() {
+		return menuAction.GetStateDown(SteamVR_Input_Sources.Any);
+	}
+	public bool GetMenu(SteamVR_Input_Sources hand) {
+		return menuAction.GetStateDown(hand);
+	}
+
+	public bool GetLeftClick() {
+		return leftClickAction.GetStateDown(SteamVR_Input_Sources.Any);
+	}
+	public bool GetLeftClick(SteamVR_Input_Sources hand) {
+		return leftClickAction.GetStateDown(hand);
+	}
+	public bool GetRightClick() {
+		return rightClickAction.GetStateDown(SteamVR_Input_Sources.Any);
+	}
+	public bool GetRightClick(SteamVR_Input_Sources hand) {
+		return rightClickAction.GetStateDown(hand);
+	}
+	public bool GetUpClick() {
+		return upClickAction.GetStateDown(SteamVR_Input_Sources.Any);
+	}
+	public bool GetUpClick(SteamVR_Input_Sources hand) {
+		return upClickAction.GetStateDown(hand);
+	}
+	public bool GetDownClick() {
+		return downClickAction.GetStateDown(SteamVR_Input_Sources.Any);
+	}
+	public bool GetDownClick(SteamVR_Input_Sources hand) {
+		return downClickAction.GetStateDown(hand);
+	}
+}
