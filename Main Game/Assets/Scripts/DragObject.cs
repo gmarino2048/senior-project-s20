9867efc0e1b105ff53401e9afd76be8309fdade0
Christian Alexander Bonnell <cbonnell28@gmail.com>
2020-02-17 23:02:12 -0500

    Created collider system for basic one to one object snapping

---
+++ Main Game/Assets/Scripts/DragObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+
+    private Vector3 mOffSet;
+    private float mZCoord;
+
+    private void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffSet = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);  
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + mOffSet;
+    }   
+}
---
+++ Main Game/Assets/Scripts/SnapController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapController: MonoBehaviour
+{
+    Vector3 sizeOfObject;
+
+    void Start()
+    {
+        sizeOfObject = transform.parent.parent.GetComponent<Collider>().bounds.size;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "SnappableObject")
+        {
+            float sizeX = sizeOfObject.x;
+            float sizeY = sizeOfObject.y;
+            float sizeZ = sizeOfObject.z;
+
+            // Using switch in case we want to add more than one collider for an object
+            switch (this.transform.tag) 
+            {
+                case "SnapCollider":
+                    other.transform.position = new Vector3(transform.parent.parent.position.x, transform.parent.position.y + sizeY, transform.parent.position.z);
+                    break;
+            }
+        }
+    }
+}
