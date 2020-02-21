85c85fbe7176d8a1cc5685319d7df6618b09681e
Christian Alexander Bonnell <cbonnell28@gmail.com>
2020-02-21 10:46:03 -0500

    Polished the snap object interaction and created a lock and key object

--- Main Game/Assets/Scripts/DragObject.cs
+++ Main Game/Assets/Scripts/DragObject.cs
@@ -8,11 +8,13 @@ public class DragObject : MonoBehaviour
 
     private Vector3 mOffSet;
     private float mZCoord;
+    private bool held = false;
 
     private void OnMouseDown()
     {
         mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         mOffSet = gameObject.transform.position - GetMouseWorldPos();
+        held = true;
     }
 
     private Vector3 GetMouseWorldPos()
@@ -25,10 +27,16 @@ public class DragObject : MonoBehaviour
     private void OnMouseUp()
     {
         this.GetComponent<Rigidbody>().useGravity = true;
+        held = false;
     }
 
     private void OnMouseDrag()
     {
         transform.position = GetMouseWorldPos() + mOffSet;
     }   
+
+    public bool IsHeld()
+    {
+        return held;
+    }
 }
