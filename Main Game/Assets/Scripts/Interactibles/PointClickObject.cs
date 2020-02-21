bd1342d6b3edcc5c6d72df4ddae5bd06619b18d4
devin-ohlson <46634476+devin-ohlson@users.noreply.github.com>
2020-02-21 12:55:35 -0500

    Dohlson/player controller (#20)
    * Set up a mouse and keyboard player controller prefab.
    Initialized input bindings for the Vive controllers.
    
    * Updated scene for clearer examples
    
    * Added clickable interactive objects (e.g. buttons)
    Set up a demo for that in the MouseKeyboardScene
    
    * Added support for holding down/releasing PointClickObjects
    
    * Fixed problem with holding down PCOs
    Added new button in MouseKeyboardScene to demo new functionality

--- Main Game/Assets/Scripts/Interactibles/PointClickObject.cs
+++ Main Game/Assets/Scripts/Interactibles/PointClickObject.cs
@@ -7,7 +7,7 @@ using Valve.VR;
 [RequireComponent(typeof(Collider))]
 public abstract class PointClickObject : MonoBehaviour {
 	private Vector3 originalScale;
-	[SerializeField] private float scaleAmount;
+	[SerializeField] private float scaleAmount = 1.01f;
 
 	protected virtual void Start() {
 		originalScale = transform.localScale;
@@ -23,5 +23,9 @@ public abstract class PointClickObject : MonoBehaviour {
 		transform.localScale = originalScale;
 	}
 
+	//Called when the player clicks on the object
 	public abstract void Interact(HandController hand);
+
+	//Called when the player releases the click
+	public virtual void Release(HandController hand) { }
 }
