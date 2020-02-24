165d7e72248f643849ee03213d6edfb51ba8af2e
Christian Alexander Bonnell <cbonnell28@gmail.com>
2020-02-24 08:06:31 -0500

    Added functionality to check cogs for correct placement order. New bug has occured where objects no longer snap to the board

---
+++ Main Game/Assets/Scripts/CogBoardController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CogBoardController : MonoBehaviour
+{
+	[SerializeField] private GameObject[] currentCogOrderSlots;
+	[SerializeField] private GameObject[] correctCogOrder;
+
+	private void Start()
+	{
+		CogController.Connected += CheckCogOrder;
+	}
+
+	private void CheckCogOrder()
+	{
+		Debug.Log("Checking cog order");
+		bool isSolved = true;
+
+		for(int i = 0; i < currentCogOrderSlots.Length; i++)
+		{
+			if(currentCogOrderSlots[i] != correctCogOrder[i])
+			{
+				isSolved = false;
+				Debug.Log("Locked");
+			}
+		}
+
+		if (isSolved)
+		{
+			Unlock();
+		}
+	}
+
+	private void Unlock()
+	{
+		Debug.Log("Unlocked");
+	}
+}
---
+++ Main Game/Assets/Scripts/CogController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CogController : MonoBehaviour
+{
+	public static event Action Connected = delegate { };
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log(collision.gameObject.tag);
+		if(collision.gameObject.tag == "SnapCollider")
+		{
+			Connected();
+		}
+	}
+}
