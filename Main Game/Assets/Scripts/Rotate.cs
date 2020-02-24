f0a202647dc68bf8e9cc59b2a2e1506f52954152
Christian Alexander Bonnell <cbonnell28@gmail.com>
2020-02-24 01:47:14 -0500

    Converted spaces to tabs

---
+++ Main Game/Assets/Scripts/Rotate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Rotate : MonoBehaviour
+{
+	public static event Action<string, int> Rotated = delegate{};
+
+	private bool wheelIsSpinning;
+	private int currentNumber;
+	
+	void Start()
+	{
+		wheelIsSpinning = false;
+		currentNumber = 0;
+	}
+
+	private void OnMouseDown(){
+		if(!wheelIsSpinning){
+			StartCoroutine("RotateWheel");
+		}
+	}
+
+	private IEnumerator RotateWheel(){
+		wheelIsSpinning = true;
+
+		for (int i = 0; i < 15; i++){
+			transform.Rotate(0f, -3f, 0f);
+			yield return new WaitForSeconds(0.01f);
+		}
+
+		wheelIsSpinning = false;
+		currentNumber++;
+
+		if (currentNumber > 7){
+			currentNumber = 0;
+		}
+
+		Rotated(name, currentNumber);
+	}
+}
