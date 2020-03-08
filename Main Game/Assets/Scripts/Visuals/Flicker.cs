185f2299493c5f52cca117693acff70b2eb667d3
Guy Marino <gxm262@case.edu>
2020-03-08 13:53:54 -0400

    Add prime selection Revert all private values to actual objects

--- Main Game/Assets/Scripts/Visuals/Flicker.cs
+++ Main Game/Assets/Scripts/Visuals/Flicker.cs
@@ -6,18 +6,28 @@ using UnityEngine;
 
 public class Flicker : MonoBehaviour
 {
+    public int RandomSeed { get; set; }
     public int NumberPrimes { get; set; }
     public float FlickerSpeed { get; set; }
 
-    private IEnumerable<Light> campfireLights;
-    private IEnumerable<int> primes;
+    private System.Random random;
+
+    private List<Light> campfireLights;
+    private List<int> primes;
+
+    private List<float> nextFlicker;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        campfireLights = GetCampfireLights();
-        primes = GetPrimes(NumberPrimes);
+        random = new System.Random(RandomSeed);
+
+        campfireLights = new List<Light>(GetCampfireLights());
+        primes = new List<int>(GetPrimes(NumberPrimes));
+
+        // Select primes from the list for each light
+        var selectedPrimes = RandomSelect<int>(campfireLights.Count(), primes, random);
     }
 
     // Update is called once per frame
@@ -64,6 +74,7 @@ public class Flicker : MonoBehaviour
         return primeList;
     }
 
+    // Check to see if a number is prime given a list of other primes
     private bool IsPrime(int candidate, IEnumerable<int> previousValues)
     {
         var sqrt = (int)Math.Sqrt(candidate);
@@ -77,11 +88,28 @@ public class Flicker : MonoBehaviour
         return true;
     }
 
+    // Check whether a float is within a given tolerance range
+    // More stable than a direct float comparison
     private bool Within(float comp, float target, float tolerance)
     {
         var upper = target + tolerance;
         var lower = target - tolerance;
 
         return lower <= comp && comp <= upper;
+    }
+
+    private IEnumerable<T> RandomSelect<T>(int count, IList<T> list, System.Random random = null)
+    {
+        var retVal = new List<T>(count);
+        var randomImpl = random ?? new System.Random();
+
+        for (var i = 0; i < count; i++)
+        {
+            int idx = random.Next(list.Count());
+            retVal[i] = list[idx];
+            list.RemoveAt(idx);
+        }
+
+        return retVal;
     }
 }
