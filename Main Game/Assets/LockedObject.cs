e00b6662cd10e631eb15320b8362f29c5e601442
Christian Alexander Bonnell <cbonnell28@gmail.com>
2020-04-06 11:10:44 -0400

    Updated lock and key system to work with fps controls

---
+++ Main Game/Assets/LockedObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedObject : InteractiveObject
+{
+	[SerializeField]
+	private bool locked;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+		base.Start();
+		KeyObject.Unlocked += checkUnlocked;
+    }
+
+	private void checkUnlocked(bool unlocked, GameObject lockedObject)
+	{
+		if(gameObject == lockedObject)
+		{
+			locked = false;
+			Debug.Log("Unlocked " + lockedObject.name);
+		}
+	}
+
+	public bool isLocked()
+	{
+		return locked;
+	}
+
+	public override void Interact(Transform handTF)
+	{
+		Debug.Log("Lock is " + locked);
+	}
+}
