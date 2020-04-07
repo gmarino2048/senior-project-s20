2b8da2cf11057c579247383a54997babb830bae6
Christian Alexander Bonnell <cbonnell28@gmail.com>
2020-04-07 14:52:36 -0400

    Cleaned up CogPlacement functionality

--- Main Game/Assets/Scripts/Interactibles/Cog Board/CogPlacement.cs
+++ Main Game/Assets/Scripts/Interactibles/Cog Board/CogPlacement.cs
@@ -5,6 +5,7 @@ using System;
 
 public class CogPlacement : ObjectPlacementVolume
 {
+	public int slot;
 	public InteractiveObject requiredInteractiveObject;
 	public static event Action<int, bool> CogPlaced = delegate { };
 	public override void PlacementTrigger(InteractiveObject interactiveObject)
@@ -12,7 +13,6 @@ public class CogPlacement : ObjectPlacementVolume
 		Debug.Log("Required interactive object: " + requiredInteractiveObject);
 		Debug.Log("Placed interactive object: " + interactiveObject);
 		Debug.Log("Correct object placed: " + (requiredInteractiveObject == interactiveObject));
-		int slot = int.Parse(gameObject.name.Substring(gameObject.name.Length - 1));
 		CogPlaced(slot, (requiredInteractiveObject == interactiveObject));
 	}
 }
