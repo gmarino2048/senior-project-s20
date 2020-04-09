7e0a3b037d27a63a2fbc76c83de6a54c9f32b139
Guy Marino <gxm262@case.edu>
2020-04-08 20:57:11 -0400

    Gmarino/flicker (#43)
    * Model initial campfire
    
    * Move files around for organization
    
    * Add campfire UV
    
    * Apply image texture to fire
    
    * Add campfire model to main game
    
    * Reimport campfire
    
    * Add fire material in unity
    
    * Mess around with meta files a bit
    
    * Add flicker portions to campfire
    
    * Integrate internal fire material
    
    * Don't have the campfire cast shadows
    
    * Add lights to campfire
    
    * Some housekeeping stuff
    
    * Update terrain and lake
    
    * Begin staging the campfire scene
    
    * Add a few simple materials
    
    * Start work on flicker script
    
    * Add prime generation and tolerance comparison
    
    * Add prime selection
    Revert all private values to actual objects
    
    * Add flicker script to new namespace
    
    * Update variable names with proper conventions
    
    * Finish rough version of flicker script
    
    * Get flicker script kinda working
    
    It still looks really janky, but hopefully I'll have it fixed with this next patch
    
    * Make flicker script look more natural
    
    The flicker script looks a lot more natural now, It's doing a lot of stuff but the ramp makes it look a whole lot better
    
    * Add base values to the flicker script
    
    These values make the flicker behavior look pretty natural, so I hard-coded them into the file for a bit.
    
    * Remove unused imports

---
+++ Main Game/Assets/Scripts/Visuals/Flicker.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Visuals
+{
+    public class Flicker : MonoBehaviour
+    {
+        public int randomSeed = 2000;
+        public int numberPrimes = 20;
+        public float flickerSpeed = 1.0f;
+        public float maxFlickerDuration = 0.5f;
+
+        private System.Random _random;
+
+        private List<Light> _campfireLights;
+        private List<int> _primes;
+        private List<FlickerInterval> _intervals;
+
+        private List<float> _nextFlicker;
+
+        class FlickerInterval
+        {
+            public Light light { get; }
+            public float interval { get; }
+            public float counter { get; set; }
+
+            private readonly float _maxFlickerDuration;
+            private readonly Random _random;
+            private const float rampTime = 0.05f;
+
+            public FlickerInterval(Light light, float interval, float maxFlickerDuration, Random random = null)
+            {
+                this.light = light;
+                this.interval = interval;
+                counter = 0;
+
+                _maxFlickerDuration = maxFlickerDuration;
+                _random = random ?? new Random();
+            }
+
+            public IEnumerator Increment(float deltaTime)
+            {
+                counter += deltaTime;
+                if (counter > interval)
+                {
+                    counter = 0;
+                    yield return DoFlicker();
+                }
+            }
+
+            private IEnumerator DoFlicker()
+            {
+                var duration = _maxFlickerDuration * (float)_random.NextDouble();
+
+                var currentIntensity = light.intensity;
+                var finalIntensity = currentIntensity  * 0.75f;
+                
+                yield return RampDown(currentIntensity, finalIntensity);
+                yield return new WaitForSeconds(duration);
+                yield return RampUp(finalIntensity, currentIntensity);
+
+                // Need to set this so because the scene just keeps getting dimmer and dimmer
+                light.intensity = currentIntensity;
+            }
+
+            private IEnumerator RampDown(float initialIntensity, float finalIntensity)
+            {
+                if (initialIntensity < finalIntensity)
+                {
+                    yield return RampUp(initialIntensity, finalIntensity);
+                }
+                else
+                {
+                    var currentIntensity = initialIntensity;
+                    while (currentIntensity > finalIntensity)
+                    {
+                        var span = initialIntensity - finalIntensity;
+                        var decAmount = span * (Time.deltaTime / rampTime);
+
+                        light.intensity -= decAmount;
+                        currentIntensity -= decAmount;
+                        yield return new WaitForEndOfFrame();
+                    }
+                }
+            }
+
+            private IEnumerator RampUp(float initialIntensity, float finalIntensity)
+            {
+                if (initialIntensity > finalIntensity)
+                {
+                    yield return RampDown(initialIntensity, finalIntensity);
+                }
+                else
+                {
+                    var currentIntensity = initialIntensity;
+                    while (currentIntensity < finalIntensity)
+                    {
+                        var span = finalIntensity - currentIntensity;
+                        var incAmount = span * (Time.deltaTime / rampTime);
+
+                        light.intensity += incAmount;
+                        currentIntensity += incAmount;
+                        yield return new WaitForEndOfFrame();
+                    }
+                }
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _random = new System.Random(randomSeed);
+            _campfireLights = new List<Light>(GetCampfireLights());
+
+            // Avoid light flicker overlapping
+            if (numberPrimes < _campfireLights.Count * 2)
+                numberPrimes = _campfireLights.Count * 2;
+            
+            _primes = new List<int>(GetPrimes(numberPrimes));
+
+            // Select primes from the list for each light
+            _intervals = new List<FlickerInterval>();
+            foreach (var campfireLight in _campfireLights)
+            {
+                var interval = GetInterval(_primes, _random);
+                _intervals.Add(new FlickerInterval(
+                    campfireLight,
+                    interval,
+                    maxFlickerDuration,
+                    _random));
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            foreach (var flickerInterval in _intervals)
+            {
+                
+                StartCoroutine(flickerInterval.Increment(Time.deltaTime * flickerSpeed));
+            }
+        }
+        
+        // Get the interval for each light
+        private float GetInterval(List<int> primes, Random random = null)
+        {
+            random = random ?? new Random();
+            var selected = new SortedSet<int>(RandomSelect(2, primes, random));
+            
+            // List of primes should be unique so standard removal should work
+            foreach (var item in selected)
+            {
+                primes.Remove(item);
+            }
+
+            return selected.Max / (float) selected.Min;
+        }
+
+        // Fetch all the point lights in the object
+        private IEnumerable<Light> GetCampfireLights()
+        {
+            return gameObject.GetComponentsInChildren<Light>().Where(
+                light => light.type == LightType.Point);
+        }
+
+        // Get a list of all the intervals that shouldn't overlap
+        private IEnumerable<int> GetPrimes(int numPrimes)
+        {
+            int count;
+            var primeList = new List<int>();
+
+            int nextPrime = 3;
+            while ((count = primeList.Count) <= numPrimes)
+            {
+                switch (count)
+                {
+                    case 0:
+                        primeList.Add(2);
+                        break;
+                    case 1:
+                        primeList.Add(3);
+                        break;
+                    default:
+                        if (IsPrime(nextPrime, primeList))
+                        {
+                            primeList.Add(nextPrime);
+                        }
+
+                        break;
+                }
+
+                nextPrime += 2;
+            }
+
+            return primeList;
+        }
+
+        // Check to see if a number is prime given a list of other primes
+        private bool IsPrime(int candidate, IEnumerable<int> previousValues)
+        {
+            var sqrt = (int) Math.Sqrt(candidate);
+
+            foreach (var prime in previousValues)
+            {
+                if (prime > sqrt) return true;
+                if (candidate % prime == 0) return false;
+            }
+
+            return true;
+        }
+
+        // Select `count` elements randomly from list
+        private IEnumerable<T> RandomSelect<T>(int count, IList<T> list, System.Random random = null)
+        {
+            var retVal = new List<T>(count);
+            var randomImpl = random ?? new System.Random();
+
+            for (var i = 0; i < count; i++)
+            {
+                var idx = random.Next(list.Count());
+                retVal.Add(list[idx]);
+                list.RemoveAt(idx);
+            }
+
+            return retVal;
+        }
+    }
+}
