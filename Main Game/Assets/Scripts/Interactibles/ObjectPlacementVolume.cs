582f2259a85cb0e557c70ab08900e5e2afcef80e
Christian Alexander Bonnell <cbonnell28@gmail.com>
2020-04-10 23:03:17 -0400

    Using a list of required objects did not work so I have switched to using a required tag and will try implementing this soon

--- Main Game/Assets/Scripts/Interactibles/ObjectPlacementVolume.cs
+++ Main Game/Assets/Scripts/Interactibles/ObjectPlacementVolume.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public abstract class ObjectPlacementVolume : MonoBehaviour {
 	public List<GameObject> requiredObjects;
+	public string requiredTag;
 
 	public abstract void PlacementTrigger(InteractiveObject gameObject);
 }
