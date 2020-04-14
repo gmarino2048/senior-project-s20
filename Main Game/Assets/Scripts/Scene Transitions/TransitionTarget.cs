273037a9d3d2d03bfde25f05623a7a9889028cc8
Guy Marino <gxm262@case.edu>
2020-04-13 20:11:29 -0400

    Gmarino/scene transitions (#58)
    * Initialize required scene transition files
    
    * Add controller objects and color shifting for magnifying glass
    
    * Update scene transition scripts
    
    * Remove unused C# file

---
+++ Main Game/Assets/Scripts/Scene Transitions/TransitionTarget.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class TransitionTarget : MonoBehaviour
+{
+    [SerializeField] private float groundOffset = 0.01f;
+    public Vector3 spawnPoint { get; private set; }
+    
+    private void Start()
+    {
+        if (Physics.Raycast(gameObject.transform.position, Vector3.down, out var hit))
+        {
+            var hitPosition = hit.point;
+            spawnPoint = new Vector3(
+                hitPosition.x,
+                hitPosition.y + groundOffset,
+                hitPosition.z
+            );
+        }
+        else throw new InvalidConstraintException("Could not find the floor to establish spawn point");
+    }
+}
