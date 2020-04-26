fa66bc22a2cf30b80f990a42794da4732a551a5c
devin-ohlson <46634476+devin-ohlson@users.noreply.github.com>
2020-04-26 18:19:00 -0400

    Set up item respawn zones in all scenes except campsite and witch hut (#90)
    * Set up item respawn zones in all scenes except campsite and witch hut
    
    * Excludes potions from respawning

---
+++ Main Game/Assets/Scripts/OOBZone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OOBZone : MonoBehaviour {
+    [SerializeField] private Transform respawnPoint;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.GetComponent<KeyboardPlayerController>() == null && other.gameObject.GetComponent<PotionItem>() == null) {
+            other.gameObject.transform.position = respawnPoint.position;
+            other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
