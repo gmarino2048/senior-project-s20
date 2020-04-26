123da2a4aa6a58006cd01ac34636a3fa0dca2526
Christian Alexander Bonnell <cbonnell28@gmail.com>
2020-04-25 23:42:20 -0400

    Added chest manager to help handle transition zones

---
+++ Main Game/Assets/Scripts/Interactibles/Chest1Manager1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest1Manager1 : PotionInteractionObject
+{
+	[SerializeField]
+	private GameObject ObjectsToActivate;
+
+	public override IEnumerator PotionEffects()
+	{
+		Debug.Log("Should be enabling");
+		ObjectsToActivate.SetActive(true);
+		this.gameObject.SetActive(false);
+		yield return new WaitForSeconds(0);
+	}
+}
