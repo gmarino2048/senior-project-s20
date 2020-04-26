10e72679c2034561fc654acc51a555e402afba70
Christian Alexander Bonnell <cbonnell28@gmail.com>
2020-04-26 13:29:09 -0400

    Cleaned up code and removed unused scripts

--- Main Game/Assets/Scripts/Interactibles/LockedObject.cs
+++ Main Game/Assets/Scripts/Interactibles/LockedObject.cs
@@ -8,9 +8,6 @@ public class LockedObject : ObjectPlacementVolume
 
 	public override void PlacementTrigger(InteractiveObject placedObject)
 	{
-		Debug.Log("Unlocked = " + (placedObject,gameObject == requiredObject.gameObject));
-		Debug.Log("Placed object: " + placedObject.gameObject);
-		Debug.Log("Required object: " + requiredObject.gameObject);
 		if (requiredObject.gameObject == placedObject.gameObject)
 		{
 			gameObject.SetActive(false);
