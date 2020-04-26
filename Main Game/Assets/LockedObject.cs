f17ec0802f0b346c4197cfd76ff6db6b05deaffc
Christian Alexander Bonnell <cbonnell28@gmail.com>
2020-04-26 13:06:31 -0400

    Integrated new lock and key system

--- Main Game/Assets/LockedObject.cs
+++ Main Game/Assets/LockedObject.cs
@@ -8,7 +8,10 @@ public class LockedObject : ObjectPlacementVolume
 
 	public override void PlacementTrigger(InteractiveObject placedObject)
 	{
-		if(requiredObject == placedObject)
+		Debug.Log("Unlocked = " + (placedObject,gameObject == requiredObject.gameObject));
+		Debug.Log("Placed object: " + placedObject.gameObject);
+		Debug.Log("Required object: " + requiredObject.gameObject);
+		if (requiredObject.gameObject == placedObject.gameObject)
 		{
 			gameObject.SetActive(false);
 			toActivate.SetActive(true);
