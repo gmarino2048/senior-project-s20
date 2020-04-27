b35fa7cf2871a93d15dfa61bdb4da91f80e43976
Guy Marino <gxm262@case.edu>
2020-04-27 17:47:57 -0400

    Update main menu

--- Main Game/Assets/Scripts/GameFlowController.cs
+++ Main Game/Assets/Scripts/GameFlowController.cs
@@ -1,18 +1,80 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class GameFlowController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Menu settings")]
+    [SerializeField] private ScenicCameraRig cameraRig;
+    [SerializeField] private MenuController menu;
+    private Camera scenicCamera;
+
+    [Header("Player Settings")]
+    [SerializeField] private KeyboardPlayerController player;
+    private Camera playerCamera;
+    private Transform initialPlayerTransform;
+
+    [Header("Fade Settings")]
+    [SerializeField] private float fadeDuration;
+    [SerializeField] private float contemplationTime;
+    private FadeController fadeController;
+
+    private void Awake()
     {
+        scenicCamera = cameraRig.GetComponentInChildren<Camera>();
+        playerCamera = player.GetComponentInChildren<Camera>();
         
+        initialPlayerTransform = player.gameObject.transform;
+
+        fadeController = FindObjectOfType<FadeController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        SwitchToMenu();
+    }
+
+    private void SwitchToPlayer()
+    {
+        playerCamera.enabled = true;
+
+        cameraRig.active = false;
+        scenicCamera.enabled = false;
         
+        player.SetPlayerMovementLock(false);
+    }
+
+    private void SwitchToMenu()
+    {
+        cameraRig.active = true;
+        scenicCamera.enabled = true;
+        
+        playerCamera.enabled = false;
+        
+        player.SetPlayerMovementLock(true);
+    }
+
+    private void ResetPlayer()
+    {
+        var playerObject = player.gameObject;
+        playerObject.transform.position = initialPlayerTransform.position;
+        playerObject.transform.rotation = initialPlayerTransform.rotation;
+    }
+
+    public IEnumerator StartGame()
+    {
+        yield return fadeController.FadeOut(Color.black, fadeDuration);
+        SwitchToPlayer();
+        yield return fadeController.FadeIn(fadeDuration);
+    }
+
+    public IEnumerator EndGame()
+    {
+        yield return fadeController.FadeOut(Color.white, fadeDuration);
+        // TODO: CHRISTIAN PUT THE CREDITS HERE
+        yield return new WaitForSeconds(contemplationTime);
+        SwitchToMenu();
+        ResetPlayer();
+        yield return fadeController.FadeIn(fadeDuration);
     }
 }
--- Main Game/Assets/Scripts/Visuals/MenuController.cs
+++ Main Game/Assets/Scripts/Visuals/MenuController.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    private GameFlowController flowController;
+
+    private void Awake()
     {
-        
+        flowController = FindObjectOfType<GameFlowController>();
+        playButton.onClick.AddListener(OnPlay);
+        quitButton.onClick.AddListener(OnQuit);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnPlay()
     {
-        
+        StartCoroutine(
+            flowController.StartGame());
+    }
+
+    private void OnQuit()
+    {
+        Application.Quit(0);
     }
 }
