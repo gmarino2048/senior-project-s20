f4183438918bfe4e9ffe1c075663eca61bc37eec
Christian Alexander Bonnell <33079068+cbonnell28@users.noreply.github.com>
2020-04-27 21:28:42 -0400

    Added rolling credits on beating the game (#116)

---
+++ Main Game/Assets/Scripts/Visuals/CreditsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsController : MonoBehaviour
+{
+	[SerializeField] private GameObject credits;
+	public GameObject start;
+	public GameObject end;
+
+	private GameFlowController flowController;
+
+	public void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			StartCoroutine(RollCredits());
+		}
+	}
+
+	private void Awake()
+	{
+		flowController = FindObjectOfType<GameFlowController>();
+	}
+
+	public IEnumerator RollCredits()
+	{
+		Debug.Log("Rolling credits");
+		RectTransform creditPosition = credits.GetComponent<RectTransform>();
+		Vector3 originalPosition = creditPosition.transform.position;
+
+		while(Vector3.Distance(creditPosition.transform.position, end.transform.position) > 0)
+		{
+			creditPosition.transform.position = Vector3.Lerp(creditPosition.transform.position, end.transform.position, .001f);
+			yield return new WaitForFixedUpdate();
+		}
+	}
+}
